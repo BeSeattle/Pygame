a546951a3ba60aa1b9bbad66a73165ede472644a
BeSeattle <1060927292@qq.com>
2017-01-21 21:28:37 +0800

    .

--- game.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-keys = [False， False， False， False]
-playerpos=[100，100] 
--- playgame.py
+++ playgame.py
@@ -2,35 +2,60 @@
 from __future__ import unicode_literals
 import pygame
 from pygame.locals import *
-  
+import math 
+
 # 2 - Initialize the game
 pygame.init()
 pygame.display.set_caption('小兔子大战')
 size = width,height = 1000,600
-screen=pygame.display.set_mode(size)`
+screen=pygame.display.set_mode(size)
 coordinate = coordinate_x,coordinate_y = 100,100
-
+keys = [False, False, False, False]
+playerpos=[100,100] 
+acc=[0,0]
+arrows=[]
 # 3 - Load images
 player = pygame.image.load("resources/images/dude.png")
 grass = pygame.image.load("resources/images/grass.png")
 castle = pygame.image.load("resources/images/castle.png")
-
+arrow = pygame.image.load("resources/images/bullet.png")
 # 4 - keep looping through,这样才能保持兔子窗口一直显示
-for game in range(1,50):
+while True:
     # 5 - clear the screen before drawing it again
     screen.fill(0)
-
-    # 6 - draw the screen elements,(**,**)代表的是元素的坐标
+    #背景渲染
     for x in range(int(width/grass.get_width())+1):
         for y in range(int(height/grass.get_height())+1):
             screen.blit(grass, (x*100, y*100))
-
-    screen.blit(castle, (0, 30))
-    screen.blit(castle, (0, 135))
-    screen.blit(castle, (0, 240))
-    screen.blit(castle, (0, 345))
-    screen.blit(castle, (0, 450))
-    screen.blit(player, (coordinate))
+            screen.blit(castle, (0, 30))
+            screen.blit(castle, (0, 135))
+            screen.blit(castle, (0, 240))
+            screen.blit(castle, (0, 345))
+            screen.blit(castle, (0, 450))
+    # 6 - draw the screen elements,(**,**)代表的是元素的坐标
+    
+    # 6.1 - Set player position and rotation
+    position = pygame.mouse.get_pos()
+    angle = math.atan2(position[1]-(playerpos[1]+32), position[0]-(playerpos[0]+26))
+    #360/2π约等于57.29,angle*57.29是为了把弧度转换为角度
+    playerrot = pygame.transform.rotate(player,  360-angle*57.29)
+    playerpos1 = (playerpos[0]-playerrot.get_rect().width/2,  playerpos[1]-playerrot.get_rect().height/2)
+    screen.blit(playerrot, playerpos1)
+    
+    # 6.2 - Draw arrows
+    for bullet in arrows:
+        index=0
+        velx=math.cos(bullet[0])*100
+        vely=math.sin(bullet[0])*100
+        bullet[1]+=velx
+        bullet[2]+=vely
+        if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
+            arrows.pop(index)
+        index+=1
+        for projectile in arrows:
+            arrow1 = pygame.transform.rotate(arrow, 360-projectile[0]*57.29)
+            screen.blit(arrow1, (projectile[1], projectile[2]))
+    
     # 7 - update the screen
     pygame.display.flip()
     # 8 - loop through the events
@@ -40,3 +65,35 @@ for game in range(1,50):
             # if it is quit the game
             pygame.quit() 
             exit()
+        
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            position=pygame.mouse.get_pos()
+            acc[1]+=1
+            arrows.append([math.atan2(position[1]-(playerpos1[1]+32),position[0]-(playerpos1[0]+26)),playerpos1[0]+32,playerpos1[1]+32])
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key==K_w:
+                keys[0]=True
+            elif event.key==K_a:
+                keys[1]=True
+            elif event.key==K_s:
+                keys[2]=True
+            elif event.key==K_d:
+                keys[3]=True
+        if event.type == pygame.KEYUP:
+            if event.key==pygame.K_w:
+                keys[0]=False
+            elif event.key==pygame.K_a:
+                keys[1]=False
+            elif event.key==pygame.K_s:
+                keys[2]=False
+            elif event.key==pygame.K_d:
+                keys[3]=False
+        if keys[0]:
+            playerpos[1]-=20
+        elif keys[2]:
+            playerpos[1]+=20
+        if keys[1]:
+            playerpos[0]-=20
+        elif keys[3]:
+            playerpos[0]+=20
